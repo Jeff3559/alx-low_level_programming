63b577bc80a7b353a0e558b2b55e14ae495b74f7
Martin Inalegwu <inalegwumartin@gmail.com>
2022-09-08 22:00:30 -0700

    task101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<unistd>
+/**
+ * main-print"and that piece of art is useful"-dora korpar,2015-10-09",
+ * followed by a new line, to stand error.
+ *
+ * Return:Always 1.
+ */
+int main(void)
+{
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
+	return (1);
+}
