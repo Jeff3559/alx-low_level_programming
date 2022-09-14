dbd0ed474e9099d4cd44353b5ef7f63429b6fb8c
Martin Inalegwu <inalegwumartin@gmail.com>
2022-09-13 20:16:47 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign -> prints sign base on condition
+ * print_sign -> print sign base on condition
  * @n: argument passed
  *
  * Return: 1, 0, -1
@@ -11,7 +11,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		return ('1');
 	}
 	else if (n == 0)
 	{
