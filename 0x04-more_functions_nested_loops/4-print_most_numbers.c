c59a79b043beaefc84ac6f0bb8b0a4cc61aab001
Martin Inalegwu <inalegwumartin@gmail.com>
2022-09-16 07:00:51 -0700

    task7

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,11 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = '0'; n <= 9; n++)
 	{
-		if ((n == 50) || (n == 52))
+		if (n != '2' || n != '4')
 		{
-			_putchar(n);
+			_putchar('n');
 		}
 		_putchar(10);
 	}
