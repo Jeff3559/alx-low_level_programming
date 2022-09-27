d0bf8db27a677cd5911f14febb76e2295150cbfc
Martin Inalegwu <inalegwumartin@gmail.com>
2022-09-26 20:34:22 -0700

    task3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn -> gets the length of a prefix substring
+ * @s: the string to be searched
+ * @accept: the prefix to be measured
+ * Return: the number  of byte in s which consist only bytes from accet
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int b = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				b++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (b);
+		}
+		s++;
+	}
+	return (b);
+}
