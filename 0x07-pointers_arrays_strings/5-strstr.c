d48fb34039688b5837e67985531abbd1b3252300
Martin Inalegwu <inalegwumartin@gmail.com>
2022-09-26 21:28:32 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
  * _strstr -> locate a substring
  * @haystack: the string to be searched
  * @needle: the substring to be located
- * Return: pointer to the beginning of the substring
+ * Return: a pointer to the beginning of the located substring, or NULL if the substring is not found.
  */
 char *_strstr(char *haystack, char *needle)
 {
