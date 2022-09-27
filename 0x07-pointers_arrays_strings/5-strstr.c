08224c4768d39889b24e66609617efeece0e6dc6
Martin Inalegwu <inalegwumartin@gmail.com>
2022-09-26 22:25:18 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,7 +19,7 @@ char *_strstr(char *haystack, char *needle)
 		if (haystack[i] == needle[i])
 		{
 			do {
-				if (haystack[i + 1] == '0')
+				if (haystack[i + 1] == '\0')
 					return (haystack);
 				i++;
 			} while (haystack[i] == needle[i]);
