c25e9a27698518b541c9377c879e80f1830ed88b
Martin Inalegwu <inalegwumartin@gmail.com>
2022-09-29 03:26:07 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse
+ * _print_rev_recursion -> Prints a string in reverse
  * @s: the string to be printed
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
-		 _print_rev_recursion(s + 1);
-		 _putchar(*s);
-	}
+	s = "lpa\0";
+
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
