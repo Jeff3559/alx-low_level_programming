b34d00aca268776c28aeb4135e4e68818979694a
Martin Inalegwu <inalegwumartin@gmail.com>
2022-09-29 22:07:55 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,6 +11,7 @@ int is_prime_number(int n)
 	return (check_prime(n, 1));
 }
 
+
 /**
  * check_prime -> it check prime numbers
  * @n: number
@@ -25,5 +26,5 @@ int check_prime(int n, int i)
 		return (0);
 	if ((n / i) < i)
 		return (1);
-	return (check prime(n, i + 1));
+	return (check_prime(n, i + 1));
 }
