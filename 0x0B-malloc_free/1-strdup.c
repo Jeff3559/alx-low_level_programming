7e7b270dd5bf82e756a5156e9b87b7e29304f4da
Martin Inalegwu <inalegwumartin@gmail.com>
2022-10-06 12:19:45 -0700

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,26 +8,26 @@
  */
 char *_strdup(char *str)
 {
-        unsigned int i;
-        int count = 0;
-        char *dest;
+	unsigned int i;
+	int count = 0;
+	char *dest;
 
-        if (str == NULL)
-                return (NULL);
-        for (i = 0; str[i]; i++)
-        {
-                count++;
-        }
-        count += 1;
-        dest = malloc(count * sizeof(char));
-        if (dest == NULL)
-        {
-                return (NULL);
-        }
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                dest[i] = str[i];
-        }
-        dest[i] = str[i];
-        return (dest);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+		count++;
+	}
+	count += 1;
+	dest = malloc(count * sizeof(char));
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		dest[i] = str[i];
+	}
+	dest[i] = str[i];
+	return (dest);
 }
